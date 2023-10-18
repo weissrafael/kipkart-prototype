30871b83f35ddb1b59ed89dbbf9697823ecd933e
Rafael Weiss <weissfrontend@gmail.com>
2023-10-18 14:46:20 -0300

    scanner screen genocide

--- screens/Scanner/ScannerScreen.jsx
+++ screens/Scanner/ScannerScreen.jsx
@@ -1,69 +1,41 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
-import { ActivityIndicator, StyleSheet } from "react-native";
+import { ActivityIndicator } from "react-native";
 import { useKeepAwake } from "expo-keep-awake";
-import { TouchableOpacity } from "react-native-gesture-handler";
 import { useSelector, useDispatch } from "react-redux";
-import { AntDesign, Entypo, MaterialCommunityIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 import Scanner from "../../components/ScannerComponents/Scanner/Scanner";
 import {
-  TabsRow,
-  TotalValueText,
   Screen,
   FinishButton,
   ButtonText,
   FinishContainer,
-  TabsContainer,
-  AlertTitle,
-  TotalText,
-  LogoutTitle,
 } from "./ScannerScreen.styles";
 import ScrollToTopButton from "../../components/Common/ScrollToTopButton/ScrollToTopButton";
 import ShoppingList from "../../components/ScannerComponents/ShoppingList/ShoppingList";
-import LimitSetter from "../../components/ScannerComponents/LimitSetter/LimitSetter";
 import { colors } from "../../styles/styleGuide";
 import ErrorModal from "../../components/Common/ErrorModal/ErrorModal";
-// import AdBanner from '../../components/Common/AdBanner/AdBanner'
-import AdBubble from "../../components/Common/AdBubble/AdBubble";
-import Menu from "../../components/Common/Menu/Menu";
-import Icon from "../../components/Common/Icon/Icon";
 import {
   addNewProduct,
   addProduct,
   removeFromList,
   removeFromMissing,
   removeProduct,
-  resetAll,
   resetCart,
   resetLimit,
   resetMissingList,
-  setLimit,
 } from "../../store/actions/cart";
 import Colors from "../../constants/Colors";
 import GradientButton from "../../components/Common/GradientButton/GradientButton";
 import GenericErrorModal from "../../components/Common/Modals/GenericErrorModal/GenericErrorModal";
-import AlertModal from "../../components/Common/Modals/AlertModal/AlertModal";
-import { logout } from "../../store/actions/user";
 import TypeBarcodeButton from "../../components/ScannerComponents/ShoppingList/TypeBarcodeButton/TypeBarcodeButton";
 import InputModal from "../../components/Common/Modals/InputModal/InputModal";
-import api from "../../api/api";
 import useKeyboardIsOpen from "../../hooks/useKeyboardIsOpen";
-import { fireEvent } from "../../utils/analytics";
-import WinningModal from "../../components/Common/Modals/WinningModal/WinningModal";
-import AdModal from "../../components/Common/Modals/AdModal/AdModal";
 import useSound from "../../hooks/useSound";
 import ScannerSwitchButton from "../../components/Common/ScannerSwitchButton/ScannerSwitchButton";
 import {
   IndicatorText,
   IndicatorTextContainer,
 } from "../../components/ScannerComponents/ShoppingList/ShoppingList.styles";
-
-const cashLimit = require("../../assets/menu-images/cashLimit.png");
-const inventory = require("../../assets/menu-images/inventory.png");
-const historyImage = require("../../assets/menu-images/clock.png");
-const profile = require("../../assets/menu-images/profile.png");
-const alertImage = require("../../assets/gifs/limitalert.gif");
-const confirmLogout = require("../../assets/confirmLogout.png");
-const signupImage = require("../../assets/signupImage.png");
 
 const mockedItems = {
   7891035617959: {
@@ -83,19 +55,15 @@ function ScannerScreen({ navigation, route }) {
   const [total, setTotal] = useState(0);
   const [timeCount, setTimeCount] = useState(0); // in seconds
   const [activeTab, setActiveTab] = useState("list");
-  const [inputValue, setInputValue] = useState("");
   const [isToTopVisible, setIsToTopVisible] = useState(false);
   const [modalVisible, setModalVisible] = useState(false);
   const [limitAlertVisible, setLimitAlertVisible] = useState(false);
   const [confirmLogoutModal, setConfirmLogoutModal] = useState(false);
-  const [limitPageFocused, setLimitPageFocused] = useState(false);
   const [genericError, setGenericError] = useState(false);
-  const [loginModal, setLoginModal] = useState(false);
   const [typeBarcodeModal, setTypeBarcodeModal] = useState(false);
   const [barcodeValue, setBarcodeValue] = useState("");
   const [barcodeError, setBarcodeError] = useState("");
   const [winningModal, setWinningModal] = useState(false);
-  const [adModal, setAdModal] = useState(true);
   const [disabledButton, setDisabledButton] = useState(false);
   const [barcodeButtonLoading, setBarcodeButtonLoading] = useState(false);
   const [productIsLoading, setProductIsLoading] = useState(false);
@@ -114,7 +82,6 @@ function ScannerScreen({ navigation, route }) {
   const resetValues = useCallback(() => {
     setTimeCount(0);
     dispatch(resetLimit());
-    setInputValue("");
     dispatch(resetCart());
     dispatch(resetMissingList());
   }, [dispatch]);
@@ -188,13 +155,6 @@ function ScannerScreen({ navigation, route }) {
 
   const addToList = useCallback(
     async (product) => {
-      fireEvent(
-        "product_scanned",
-        "Scanner",
-        "success",
-        "User scanned a product",
-        product
-      );
       setActiveTab("list");
       dispatch(addNewProduct(product));
       setProductIsLoading(false);
@@ -208,13 +168,6 @@ function ScannerScreen({ navigation, route }) {
   const addItem = useCallback(
     (item) => {
       const { barcode } = item;
-      fireEvent(
-        "product_added",
-        "Scanner",
-        "success",
-        "User added a product via the plus button",
-        item
-      );
       dispatch(addProduct(barcode));
       if (missingItems[barcode]) {
         dispatch(removeFromMissing(barcode));
@@ -226,13 +179,6 @@ function ScannerScreen({ navigation, route }) {
   const removeItem = useCallback(
     (item) => {
       const { barcode } = item;
-      fireEvent(
-        "product_removed",
-        "Scanner",
-        "success",
-        "User removed a product via the minus button",
-        item
-      );
       dispatch(removeProduct(barcode));
     },
     [dispatch]
@@ -241,48 +187,10 @@ function ScannerScreen({ navigation, route }) {
   const removeItemFromList = useCallback(
     (item) => {
       const { barcode } = item;
-      fireEvent(
-        "product_removed_from_list",
-        "Scanner",
-        "success",
-        "User removed a product via the trash icon",
-        item
-      );
       dispatch(removeFromList(barcode));
     },
     [dispatch]
   );
-
-  const setNewLimit = useCallback(
-    (newLimit) => {
-      dispatch(setLimit(newLimit));
-    },
-    [dispatch]
-  );
-
-  const selectNewMarket = useCallback(() => {
-    dispatch(resetCart());
-    navigation.navigate("SelectMarket");
-  }, [dispatch, navigation]);
-
-  const handleLogout = useCallback(async () => {
-    try {
-      dispatch(resetAll());
-      dispatch(logout());
-      await navigation.navigate("Login");
-      setConfirmLogoutModal(false);
-    } catch (e) {
-      setGenericError(true);
-    }
-  }, [dispatch, navigation]);
-
-  const openLogoutModal = useCallback(() => {
-    setConfirmLogoutModal(true);
-  }, []);
-
-  const openLimitTab = useCallback(() => {
-    setActiveTab("limit");
-  }, []);
 
   const weightFormat = useCallback((w) => {
     let newWeight;
@@ -303,7 +211,6 @@ function ScannerScreen({ navigation, route }) {
 
   const addItemByBarcode = useCallback(
     (barcode) => {
-      // const marketId = market.id;
       if (disabledButton) {
         return;
       }
@@ -312,9 +219,6 @@ function ScannerScreen({ navigation, route }) {
       }
       setBarcodeButtonLoading(true);
       setDisabledButton(true);
-      // api
-      //   .get(`/api/v1/products/${barcode}?marketId=${marketId}`)
-      //   .then((resp) => {
       setBarcodeError("");
       setTimeout(() => {
         setDisabledButton(false);
@@ -343,14 +247,6 @@ function ScannerScreen({ navigation, route }) {
         .catch(() => {
           setBarcodeButtonLoading(false);
         });
-      // })
-      // .catch((error) => {
-      //   setDisabledButton(false);
-      //   setBarcodeButtonLoading(false);
-      //   if (error.response.data.result.includes("Barcode")) {
-      //     setBarcodeError("Produto não encontrado");
-      //   }
-      // });
     },
     [
       addToList,
@@ -362,145 +258,12 @@ function ScannerScreen({ navigation, route }) {
     ]
   );
 
-  const menuOptions = [
-    {
-      id: 1,
-      name: "Limite de Gastos",
-      onPress: openLimitTab,
-      image: cashLimit,
-    },
-    {
-      id: 2,
-      name: "Últimas Compras",
-      onPress: () => navigation.navigate("History"),
-      image: historyImage,
-    },
-    {
-      id: 3,
-      name: "Inventário",
-      onPress: () => console.log("inventario"),
-      image: inventory,
-    },
-    {
-      id: 4,
-      name: "Minha Conta",
-      onPress: () => console.log("profile"),
-      image: profile,
-    },
-  ];
-
-  const limitModalText = (
-    <AlertTitle fontSize={22} color={Colors.secondary}>
-      Opa, você ja gastou
-      <TotalText>
-        {" "}
-        R$
-        {Number(total.toString().match(/^\d+(?:\.\d{0,2})?/))}
-      </TotalText>
-    </AlertTitle>
-  );
-
   return (
     <Screen activeTab={activeTab}>
-      {/* <AdBanner visible={adVisible} text="Amaciante Ypê Delicado" subText="para peles sensíveis - apenas 18,49" /> */}
-      {/*<AdBubble />*/}
       <GenericErrorModal
         show={genericError}
         setGenericError={setGenericError}
       />
-      {/* LOGOUT MODAL */}
-      <AlertModal
-        setModal={setConfirmLogoutModal}
-        modalImage={confirmLogout}
-        textComponent={<LogoutTitle>Tem certeza que deseja sair?</LogoutTitle>}
-        isOpen={confirmLogoutModal}
-      >
-        <GradientButton
-          color1={colors.primary}
-          color2={colors.blueGrotto}
-          textColor={colors.secondary}
-          style={{ flex: 1, marginRight: 16 }}
-          onPress={handleLogout}
-        >
-          Confirmar
-        </GradientButton>
-        <GradientButton
-          color1={colors.primary}
-          color2={colors.blueGrotto}
-          textColor={colors.secondary}
-          style={{ flex: 1 }}
-          onPress={() => {
-            setConfirmLogoutModal(false);
-          }}
-        >
-          Cancelar
-        </GradientButton>
-      </AlertModal>
-      {/* LIMIT MODAL */}
-      <AlertModal
-        modalImage={alertImage}
-        isOpen={limitAlertVisible}
-        textComponent={limitModalText}
-        setModal={setLimitAlertVisible}
-      >
-        <GradientButton
-          onPress={() => {
-            dispatch(resetLimit());
-            setInputValue("");
-            setLimitAlertVisible(false);
-          }}
-          style={{ marginRight: 16, flex: 1 }}
-        >
-          continuar
-        </GradientButton>
-        <GradientButton
-          onPress={() => {
-            setModalVisible(false);
-            dispatch(resetLimit());
-            setLimitAlertVisible(false);
-            finishHandler();
-          }}
-          style={{ flex: 1 }}
-        >
-          finalizar
-        </GradientButton>
-      </AlertModal>
-      {/* LOGIN AND SIGNUP MODAL */}
-      <AlertModal
-        modalImage={signupImage}
-        isOpen={loginModal}
-        setModal={setLoginModal}
-        textComponent={
-          <AlertTitle>
-            Antes de continuar, você precisa entrar com uma conta.
-          </AlertTitle>
-        }
-      >
-        <GradientButton
-          color1={colors.primary}
-          color2={colors.blueGrotto}
-          textColor={colors.secondary}
-          onPress={() => {
-            setLoginModal(false);
-            navigation.navigate("Login");
-          }}
-          style={{ marginRight: 16, flex: 1 }}
-        >
-          Entrar
-        </GradientButton>
-        <GradientButton
-          color1={colors.primary}
-          color2={colors.blueGrotto}
-          textColor={colors.secondary}
-          onPress={() => {
-            setLoginModal(false);
-            navigation.navigate("Signup");
-          }}
-          style={{ flex: 1 }}
-        >
-          Cadastrar
-        </GradientButton>
-      </AlertModal>
       <ErrorModal
         isOpen={modalVisible}
         closeModal={() => setModalVisible(false)}
@@ -508,7 +271,6 @@ function ScannerScreen({ navigation, route }) {
         text="Ocorreu um erro, tente novamente. Se os problemas persistirem,
               procure um gerente"
       />
-
       {/* TYPEBARCODE MODAL */}
       <InputModal
         setModalVisible={setTypeBarcodeModal}
@@ -546,13 +308,6 @@ function ScannerScreen({ navigation, route }) {
           )}
         </GradientButton>
       </InputModal>
-
-      {/* WINNER WINNER CHICKEN DINNER */}
-
-      {/*<WinningModal setModal={setWinningModal} isOpen={winningModal} />*/}
-      {/*<AdModal setModal={setAdModal} isOpen={adModal} />*/}
-
-      {/* SCREEN */}
       {scanner && (
         <Screen>
           <Scanner
@@ -568,82 +323,19 @@ function ScannerScreen({ navigation, route }) {
           />
         </Screen>
       )}
-
-      <TabsContainer scanner={scanner} activeTab={activeTab}>
-        <TabsRow>
-          <TouchableOpacity
-            style={{
-              ...styles.tab,
-              ...styles.promotionTag,
-              ...{ backgroundColor: colors.tertiary },
-            }}
-            onPress={() => navigation.navigate("Promotions")}
-          >
-            <Entypo name="price-tag" size={36} color="white" />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              ...styles.tab,
-              ...styles.limitTab,
-              ...{ backgroundColor: colors.secondary },
-            }}
-            onPress={() => setActiveTab("menu")}
-          >
-            {!limitPageFocused && <Icon name="menu" color="white" size={36} />}
-            {limitPageFocused && (
-              <MaterialCommunityIcons
-                name="backburger"
-                size={36}
-                color="white"
-              />
-            )}
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              ...styles.tab,
-              ...{ backgroundColor: "white" },
-            }}
-            onPress={() => setActiveTab("list")}
-            isActive={activeTab === "list"}
-          >
-            <TotalValueText isActive={total > 0}>
-              R$ {total.toFixed(2)}
-            </TotalValueText>
-          </TouchableOpacity>
-        </TabsRow>
-        {activeTab === "list" && (
-          <ShoppingList
-            list={cartList}
-            simultaneousHandlers={scroll}
-            addItem={addItem}
-            removeItem={removeItem}
-            removeItemFromList={removeItemFromList}
-            missingItems={missingItems}
-            keyboardShown={keyboardShown}
-            scrollRef={scroll}
-            scrollHandler={scrollHandler}
-            productIsLoading={productIsLoading}
-          />
-        )}
-        {activeTab === "limit" && (
-          <LimitSetter
-            limit={limit}
-            inputValue={inputValue}
-            setInputValue={setInputValue}
-            setLimit={setNewLimit}
-            setActiveTab={setActiveTab}
-            setLimitPageFocused={setLimitPageFocused}
-          />
-        )}
-        {activeTab === "menu" && (
-          <Menu
-            setLoginModal={setLoginModal}
-            user={user}
-            options={menuOptions}
-          />
-        )}
-      </TabsContainer>
-      {activeTab === "list" && Object.keys(cartList).length > 0 && (
+      <ShoppingList
+        list={cartList}
+        simultaneousHandlers={scroll}
+        addItem={addItem}
+        removeItem={removeItem}
+        removeItemFromList={removeItemFromList}
+        missingItems={missingItems}
+        keyboardShown={keyboardShown}
+        scrollRef={scroll}
+        scrollHandler={scrollHandler}
+        productIsLoading={productIsLoading}
+      />
+      {Object.keys(cartList).length > 0 && (
         <FinishContainer keyboardShown={keyboardShown}>
           <FinishButton onPress={finishHandler} isActive={isActive}>
             <ButtonText>Finalizar lista</ButtonText>
@@ -652,11 +344,8 @@ function ScannerScreen({ navigation, route }) {
           <ScannerSwitchButton setScanner={setScanner} />
         </FinishContainer>
       )}
-      {activeTab === "list" && (
-        <TypeBarcodeButton setModalVisible={setTypeBarcodeModal} />
-      )}
-      {activeTab === "list" &&
-        Object.entries(cartList).length <= 0 &&
+      <TypeBarcodeButton setModalVisible={setTypeBarcodeModal} />
+      {Object.entries(cartList).length <= 0 &&
         Object.entries(missingItems).length <= 0 && (
           <IndicatorTextContainer>
             <AntDesign name="arrowleft" size={32} color={colors.secondary} />
@@ -664,29 +353,9 @@ function ScannerScreen({ navigation, route }) {
               ou aperte o botão para digitar o cód. de barras
             </IndicatorText>
           </IndicatorTextContainer>
-        )}
+      )}
     </Screen>
   );
 }
-
-const styles = StyleSheet.create({
-  tab: {
-    height: 60,
-    justifyContent: "center",
-    minWidth: 100,
-    alignItems: "center",
-    borderTopLeftRadius: 20,
-  },
-  limitTab: {
-    position: "relative",
-    left: 20,
-    paddingRight: 20,
-  },
-  promotionTag: {
-    position: "relative",
-    left: 45,
-    paddingRight: 20,
-  },
-});
 
 export default ScannerScreen;
--- screens/Scanner/ScannerScreen.styles.jsx
+++ screens/Scanner/ScannerScreen.styles.jsx
@@ -9,21 +9,6 @@ const { height, width } = Dimensions.get("window");
 
 export const Screen = styled.View`
   flex: 1;
-  background-color: ${({ activeTab }) =>
-    activeTab === "menu" ? colors.secondary : "white"};
-`;
-
-export const LogoutContainer = styled.View`
-  flex: 1;
-  width: 100%;
-`;
-
-export const AlertContainer = styled.View`
-  flex: 1;
-  width: 100%;
-  padding: 16px 24px;
-  align-items: center;
-  justify-content: space-between;
 `;
 
 export const Container = styled.View`
@@ -38,24 +23,10 @@ export const ImageContainer = styled.View`
   flex: 2;
 `;
 
-export const ModalImage = styled.Image`
-  height: 80%;
-`;
-
-export const AlertImage = styled.Image`
-  height: 60%;
-`;
-
 export const LogoutTitle = styled(BoldTitle)`
   margin-top: 16px;
   font-size: 18px;
   color: ${colors.secondary};
-`;
-
-export const Button = styled.TouchableOpacity`
-  flex: 1;
-  align-items: center;
-  justify-content: center;
 `;
 
 export const AlertTitle = styled(BoldTitle)`
@@ -68,22 +39,7 @@ export const TotalText = styled(BoldTitle)`
   font-size: 24px;
 `;
 
-export const ButtonsRow = styled.View`
-  flex-direction: row;
-  width: 100%;
-  align-items: center;
-  justify-content: center;
-`;
-
 export const Page = styled.ScrollView`
-  width: 100%;
-`;
-
-export const ListContainer = styled.View`
-  flex: 1;
-  position: relative;
-  flex-direction: column;
-  min-height: 405px;
   width: 100%;
 `;
 
@@ -93,16 +49,6 @@ export const TabsContainer = styled.View`
   ${({ scanner }) => !scanner && "flex: 1"};
 `;
 
-export const TabsRow = styled.View`
-  flex-direction: row;
-  justify-content: flex-end;
-  align-items: flex-end;
-  position: absolute;
-  top: -60px;
-  width: 100%;
-  background-color: transparent;
-`;
-
 export const ContentContainer = styled.View`
   display: flex;
   height: 100%;
@@ -110,40 +56,6 @@ export const ContentContainer = styled.View`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`;
-
-export const Tab = styled.TouchableOpacity`
-  height: 60px;
-  display: flex;
-  justify-content: center;
-  min-width: 100px;
-  align-items: center;
-  border-top-left-radius: 20px;
-`;
-
-export const TabText = styled.Text`
-  font-size: 16px;
-`;
-
-export const LimitTab = styled(Tab)`
-  ${({ isActive }) => `
-    background-color: ${isActive ? colors.green : colors.green2};
-    position: relative;
-    left: 20px;
-    padding-right: 20px;
-  `}
-`;
-
-export const LimitText = styled(TabText)`
-  ${({ isActive }) => `
-    color: ${colors.white};
-  `}
-`;
-
-export const TotalValueText = styled(TabText)`
-  ${({ isActive }) => `
-    color: ${isActive ? colors.green2 : "grey"};
-  `}
 `;
 
 export const FinishContainer = styled.View`
